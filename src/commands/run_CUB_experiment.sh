371940f7b420e1e862ffc79827659357606b561b
Yijie <yijie-zhang@uiowa.edu>
2025-04-21 23:02:44 -0500

    Original_4-21_1_gpu3_not-newrelease_elbo_K1_B32_16_48_1.0_2

--- src/commands/run_CUB_experiment.sh
+++ src/commands/run_CUB_experiment.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 OUTPUTDIR="../outputs"
-EXPERIMENT="CUB_Image_Captions_1"
-# DATADIR="../data"
+EXPERIMENT="CUB_1_test"
 DATADIR="../../../../Data/Dataset/CUB"
-EPOCHS=50
-SEED=2
-SHARED_LAT_DIM=48
-MS_LAT_DIM=16
+EPOCHS=100  # new_release: 50
+SEED=2  # new_release: 2
+SHARED_LAT_DIM=48  # new_release: 48
+MS_LAT_DIM=16  # new_release: 16
 
+OBJ="elbo"  # elbo, dreg, vcca, new_release: "dreg"
+BATCH=32  # new_release: 32
+K=1  # new_release: 10
+beta=1.0  # new_release: 1.0
+
+gpuid=3
+
+# new_release: obj="dreg", K=10, batch-size=32, beta=1.0, priorposterior="Normal"
 # Train MMVAEplus
-python train_MMVAEplus_CUB.py --experiment $EXPERIMENT --obj "dreg" --K 10 --batch-size 32 --epochs $EPOCHS \
-      --latent-dim-z $SHARED_LAT_DIM --latent-dim-w $MS_LAT_DIM --seed $SEED --beta 1.0 \
+CUDA_VISIBLE_DEVICES=${gpuid} python train_MMVAEplus_CUB.py --experiment $EXPERIMENT --obj $OBJ --K $K --batch-size $BATCH --epochs $EPOCHS \
+      --latent-dim-z $SHARED_LAT_DIM --latent-dim-w $MS_LAT_DIM --seed $SEED --beta $beta \
       --datadir $DATADIR  --outputdir $OUTPUTDIR \
       --inception_path "${DATADIR}/pt_inception-2015-12-05-6726825d.pth" \
       --priorposterior "Normal"
--- src/train_MMVAEplus_CUB.py
+++ src/train_MMVAEplus_CUB.py
@@ -72,7 +72,7 @@ if not args.experiment:
     args.experiment = model.modelName
 
 # Set up run path
-runId = str(args.latent_dim_w) + '_' + str(args.latent_dim_z) + '_' + str(args.beta) + '_' + str(args.seed)
+runId = 'Original_4-21_1_gpu3_not-newrelease_elbo_K1_B32_' + str(args.latent_dim_w) + '_' + str(args.latent_dim_z) + '_' + str(args.beta) + '_' + str(args.seed)
 experiment_dir = Path(os.path.join(args.outputdir, args.experiment, "checkpoints"))
 experiment_dir.mkdir(parents=True, exist_ok=True)
 runPath = os.path.join(str(experiment_dir), runId)
