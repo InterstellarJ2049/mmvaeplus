40913d577522e9e1053190ef11e7b6bffd0254e0
Yijie <yijie-zhang@uiowa.edu>
2025-04-21 23:07:17 -0500

    Original_4-21_2_gpu2_newrelease_dreg_K10_B32_16_48_1.0_2

--- src/commands/run_CUB_experiment.sh
+++ src/commands/run_CUB_experiment.sh
@@ -8,9 +8,9 @@ SEED=2  # new_release: 2
 SHARED_LAT_DIM=48  # new_release: 48
 MS_LAT_DIM=16  # new_release: 16
 
-OBJ="elbo"  # elbo, dreg, vcca, new_release: "dreg"
+OBJ="dreg"  # elbo, dreg, vcca, new_release: "dreg"
 BATCH=32  # new_release: 32
-K=1  # new_release: 10
+K=10  # new_release: 10
 beta=1.0  # new_release: 1.0
 
 gpuid=3
